68ffcc8c3dda5882012ecf6db011ee09f3645bfa
PanTostado8 <108308054+PanTostado8@users.noreply.github.com>
2022-06-29 16:58:49 -0500

    Add files via upload

---
+++ Web/Brain.js
@@ -0,0 +1,4 @@
+var Datos = '0.0.1'
+var Name = 'Chend'
+var Incorporate = [Name = 'Chend', Datos = 'version 0.0.1', Creator = 'Oswaldo', Info = 'Chend is create for Oswaldo']
+ console.log(Incorporate)    
